0d88b5a53bb6c0e009178b4486b80b6215261105
Mick <mickvanvliet@outlook.com>
2021-11-01 08:56:54 +0100

    added soundsystem page

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,21 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import styles from './splash.module.css'
+import Head from "next/head";
+import Header from "@components/Header";
 
 export default function Home() {
   return (
-    <div 
-      // class={['c-modal__dialog', style['no-transition'], style.popover]}
-      className={`${styles['background-splash']} container`}>
+    <div>
       <Head>
         <title>Boyos Collective</title>
-        <link href="https://fonts.googleapis.com/css2?family=Limelight&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Limelight&display=swap"
+          rel="stylesheet"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="container mx-auto">
         <Header title="Boyos Collective" />
       </main>
-
-      <Footer />
     </div>
-  )
+  );
 }
---
+++ pages/soundsystem.js
@@ -0,0 +1,7 @@
+export default function Soundsystem() {
+  return (
+    <div className="container mx-auto">
+      <h1>Soundsystem</h1>
+    </div>
+  );
+}
