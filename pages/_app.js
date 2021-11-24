751335a3af9139eb2d287f0d2f815f3b52a8deb1
Mick <mickvanvliet@outlook.com>
2021-11-24 15:33:23 +0100

    removed tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
-import '@styles/globals.css'
-import 'tailwindcss/tailwind.css'
+import "@styles/globals.css";
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default Application
+export default Application;
