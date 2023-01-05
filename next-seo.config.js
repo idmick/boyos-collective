85c6b1936029c4f0b2a23c35174430e4aacb5a77
Mick <mickvanvliet@outlook.com>
2023-01-05 14:07:07 +0100

    chore: updated website description

--- next-seo.config.js
+++ next-seo.config.js
@@ -4,14 +4,14 @@ const defaultSEOConfig = {
   titleTemplate: "%s |",
   defaultTitle: "Boyos Collective",
   description:
-    "An Amsterdam based creative collective (Music, Art and Merch) that stems from a close group of friends. Hence boyos, which is an old Irish term for good friend / brother",
+    `Boyos Collective is a vibrant Amsterdam-based community of artists and creators. At the heart of Boyos is a close-knit group of friends, united by their shared passion for music, art, and fashion. The name "Boyos" itself is derived from an old Irish term meaning "good friend" or "brother," perfectly capturing the sense of camaraderie and collaboration that drives the collective forward. Whether through their music, their art, or their merchandise, Boyos Collective is dedicated to sharing their unique vision with the world, and bringing people together through the power of creativity and self-expression.`,
   canonical: "https://boyoscollective.nl",
   alt: "boyoscollective.nl",
   openGraph: {
     url: "https://boyoscollective.nl",
     title: "Boyos Collective",
     description:
-      "An Amsterdam based creative collective (Music, Art and Merch) that stems from a close group of friends. Hence boyos, which is an old Irish term for good friend / brother",
+      `Boyos Collective is a vibrant Amsterdam-based community of artists and creators. At the heart of Boyos is a close-knit group of friends, united by their shared passion for music, art, and fashion. The name "Boyos" itself is derived from an old Irish term meaning "good friend" or "brother," perfectly capturing the sense of camaraderie and collaboration that drives the collective forward. Whether through their music, their art, or their merchandise, Boyos Collective is dedicated to sharing their unique vision with the world, and bringing people together through the power of creativity and self-expression.`,
     images: [
       {
         url: "https://www.boyoscollective.nl/images/Boyos_logo_boxed.png",
--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,14 @@ const Home = () => {
   const router = useRouter();
 
   return (
-    <><NextSeo
+    <>
+    <NextSeo
       title="Boyos Collective"
-      description="A creactive collective"
+      description='Boyos Collective is a vibrant Amsterdam-based community of artists and creators. At the heart of Boyos is a close-knit group of friends, united by their shared passion for music, art, and fashion. The name "Boyos" itself is derived from an old Irish term meaning "good friend" or "brother," perfectly capturing the sense of camaraderie and collaboration that drives the collective forward. Whether through their music, their art, or their merchandise, Boyos Collective is dedicated to sharing their unique vision with the world, and bringing people together through the power of creativity and self-expression.'
       openGraph={{
         url: 'https://www.boyoscollective.nl/',
         title: 'Boyos Collective',
-        description: 'A creactive collective',
+        description: 'Boyos Collective is a vibrant Amsterdam-based community of artists and creators. At the heart of Boyos is a close-knit group of friends, united by their shared passion for music, art, and fashion. The name "Boyos" itself is derived from an old Irish term meaning "good friend" or "brother," perfectly capturing the sense of camaraderie and collaboration that drives the collective forward. Whether through their music, their art, or their merchandise, Boyos Collective is dedicated to sharing their unique vision with the world, and bringing people together through the power of creativity and self-expression.',
         siteName: 'Boyos Collective',
       }}
     />
