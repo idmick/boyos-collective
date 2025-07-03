e5991dce8d6068875a3646b51dafb20b5d0695a5
Mick <mickvanvliet@outlook.com>
2025-07-03 17:07:42 +0200

    Update soundsystem.js updated tickets links

--- pages/soundsystem.js
+++ pages/soundsystem.js
@@ -18,17 +18,6 @@ const idGen = () => {
 const linkTree = [
   {
     id: idGen(),
-    title:
-      'Tickets: Boyos Wonderland mini festival w/ Another Taste (live) & more [28-06]',
-    url: 'https://shop.weeztix.com/1e3b52ff-0405-11ec-b3c4-9e36bf7d673e/tickets?shop_code=mv8kegk9&event=50d15694-0f22-4859-8c4c-df03dff309fc',
-  },
-  {
-    id: idGen(),
-    title: 'Tickets: PBN w/ Boyos Soundsystem & Gledd [31-05]',
-    url: 'https://www.pacificamsterdam.nl/en/events/pbn-w-boyos-soundsystem-gledd',
-  },
-  {
-    id: idGen(),
     title: 'Upcoming gigs',
     url: 'https://www.instagram.com/p/DJUai8mIN_l/',
   },
