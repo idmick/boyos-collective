c6ceb169a8dc4deefa8122a9edb976f2a01d8cba
Mick van Vliet <mickvanvliet@outlook.com>
2026-05-07 16:54:38 +0200

    fix: publish sitemap for static export

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   siteUrl: 'https://www.boyoscollective.nl',
+  output: 'export',
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  exclude: ['/admin', '/admin/*'],
 }
